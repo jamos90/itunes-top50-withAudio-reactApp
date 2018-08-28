63ef3cffcdcad0d04490e293fa0c9a8ddba7d5ce
jamos90 <jamos_98@hotmail.com>
2018-08-28 17:51:15 +0100

    audio working:

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -7,9 +7,14 @@ const SongDetail =  function (props) {
     <div className = "song-item">
 
        <p>Song Title: {props.selectedSong['im:name'].label}</p>
-      <p> Artist: {props.selectedSong.title.label}</p>
-      <p> Chart Position: {props.selectedSong.index}</p>
-      <hr/>
+       <p> Artist: {props.selectedSong.title.label}</p>
+       <p> Chart Position: {props.selectedSong.index}</p>
+       <p> Album Cover:  <img src = {props.selectedSong['im:image'][2].label}/>
+       <audio controls>
+         <source src = {props.selectedSong.link[1].attributes.href} type = "audio/ogg"/>
+       </audio>
+       </p>
+       <hr/>
     </div>
   )
 }
