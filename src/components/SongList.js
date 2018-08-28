35267460c14e6cc932adf0403703e83ff9e03736
jamos90 <jamos_98@hotmail.com>
2018-08-28 17:28:08 +0100

    songlist refactored to display dropdown list

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,13 +4,24 @@ import SongDetail from './SongDetail.js';
 
 class SongList extends React.Component {
   render(){
-      const topSongs = this.props.songs.map((song, index) =>{
-        return <SongDetail song={song['im:name'].label} key={song.id.attributes['im:id']} artist={song["im:artist"].label } chartPosition = {index}/>
-      })
+      // const topSongs = this.props.songs.map((song, index) =>{
+      //   return <SongDetail song={song['im:name'].label} key={song.id.attributes['im:id']} artist={song["im:artist"].label } chartPosition = {index}/>
+      // })
+      const options = this.props.songs.map((song, index) =>{
+        return <option value = {index} key = {song.id.attributes['im:id']}>
+          {song['im:name'].label}</option>
+
+      });
 
     return(
       <div>
-        {topSongs}
+        <select
+          id ="song-selector"
+          defaultValue ="default"
+        >
+          <option disabledValue= 'default'>Select a song...</option>
+        {options}
+        </select>
       </div>
     )
   }
